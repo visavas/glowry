d6a2f8e48a88a99f53cbdcfead8eb4f2afd4be17
visavas <visavas.wut@gmail.com>
2026-02-09 17:37:05 +0700

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,5 +1,5 @@
 (function() {
-  const GITHUB_HTML_URL = "https://visavas.github.io/glowry/index.html"; // Make sure your GitHub Pages is ON
+  const GITHUB_HTML_URL = "https://visavas.github.io/glowry/";
   const LOGO = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256"><path d="M214.86,180.12a8,8,0,0,1-11,2.74L136,142.13V216a8,8,0,0,1-16,0V142.13L52.12,182.86a8,8,0,1,1-8.23-13.72L112.45,128,43.89,86.86a8,8,0,1,1,8.23-13.72L120,113.87V40a8,8,0,0,1,16,0v73.87l67.88-40.73a8,8,0,1,1,8.23,13.72L143.55,128l68.56,41.14A8,8,0,0,1,214.86,180.12Z"></path></svg>';
   
   const chatDiv = document.createElement('div');
